6bc58bf5b97b01dd488f35ef5c2ca9cc0be7bf4b
warabimochi <satomi920003@yahoo.co.jp>
2024-04-08 05:59:59 +0900

    add:更新関数に渡した引数を更新関数の中で使う例を追記

--- app/page.js
+++ app/page.js
@@ -6,15 +6,19 @@ const useStore = create((set, get) => ({
   score: 0,
   setScore10: () => set({ score: 10 }),
   scoreIncrement: () => set({ score: get().score + 1 }),
-  scoreDecrement: () => set( (state) => ({ score: state.score - 1 }) )
+  scoreDecrement: () => set( (state) => ({ score: state.score - 1 }) ),
+  getArgs: (args) => {
+    console.log(args);
+  } 
 }));
 
 export default function Home() {
-  const { score, setScore10, scoreIncrement, scoreDecrement } = useStore((state) => ({
+  const { score, setScore10, scoreIncrement, scoreDecrement, getArgs } = useStore((state) => ({
     score: state.score,
     setScore10: state.setScore10,
     scoreIncrement: state.scoreIncrement,
-    scoreDecrement: state.scoreDecrement
+    scoreDecrement: state.scoreDecrement,
+    getArgs: state.getArgs
   }), shallow);
 
   return (
@@ -23,6 +27,7 @@ export default function Home() {
     <button onClick={() => setScore10()}>scoreを10に変更するボタン</button>
     <button onClick={() => scoreIncrement()}>scoreを1増加するボタン</button>
     <button onClick={() => scoreDecrement()}>scoreを1減少するボタン</button>
+    <input onChange={(e) => getArgs(e.target.value)}></input>
     </>
   );
 }
